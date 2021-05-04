121d9ecdca47d026f8378d86f57bd26f1b04a827
RAM CHARAN GUPTA <ramcharan1004@gmail.com>
2021-05-04 23:31:14 +0530

    Commit after splash and fb

--- app/src/main/java/com/example/socialmedialogin/MainActivity.java
+++ app/src/main/java/com/example/socialmedialogin/MainActivity.java
@@ -5,52 +5,65 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.squareup.picasso.Picasso;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView info;
-    private ImageView profile;
-    private LoginButton login;
-    CallbackManager callbackManager;
+    private LoginButton login_button;
+    private CircleImageView circleImageView;
+    private TextView txtName,txtEmail;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        info = findViewById(R.id.info);
-        profile = findViewById(R.id.profile);
-        login = findViewById(R.id.login);
+        login_button = findViewById(R.id.login_button);
+        circleImageView = findViewById(R.id.profile_pic);
+        txtName = findViewById(R.id.profile_name);
+        txtEmail = findViewById(R.id.profile_email);
 
-        CallbackManager callbackManager = CallbackManager.Factory.create();
-        login.setPermissions(Arrays.asList("user_gender","user_friends"));
-        login.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+        callbackManager = CallbackManager.Factory.create();
+        login_button.setPermissions(Arrays.asList("email", "public_profile"));
+        checkLoginStatus();
+        login_button.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                info.setText("User ID: " + loginResult.getAccessToken().getUserId() +"\n" + "Auth Token: " + loginResult.getAccessToken().getToken());
-                String imageURL = "https://graph.facebook.com/" + loginResult.getAccessToken().getUserId() + "/picture?return_ssl_resources=1";
-                Picasso.get().load(imageURL).into(profile);
+
             }
 
             @Override
             public void onCancel() {
-                info.setText("Login attempt cancelled.");
+
             }
 
             @Override
             public void onError(FacebookException error) {
-                info.setText("Login attempt failed.");
+
             }
         });
 
@@ -58,7 +71,66 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode,resultCode,data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+
+            if(currentAccessToken==null)
+            {
+                txtName.setText("");
+                txtEmail.setText("");
+                circleImageView.setImageResource(0);
+                Toast.makeText(MainActivity.this,"Facebook User Logged Out",Toast.LENGTH_LONG).show();
+            }
+            else {
+                loaduserProfile(currentAccessToken);
+            }
+        }
+    };
+
+    private void loaduserProfile(AccessToken newAccessToken){
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                try {
+                    String first_name = object.getString("first_name");
+                    String last_name = object.getString("last_name");
+                    String email = object.getString("email");
+                    String id = object.getString("id");
+
+                    String image_url = "https://graph.facebook.com/" + id + "/picture?type=small&access_token=523495108641945|yy-x3xtGxxQKPpyu2HCY5AMhqSw";
+
+                    txtEmail.setText(email);
+                    txtName.setText(first_name+ " " + last_name);
+                    RequestOptions requestOptions = new RequestOptions();
+                    requestOptions.dontAnimate();
+
+                    Glide.with(MainActivity.this).load(image_url).into(circleImageView);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields","first_name,last_name,email,id");
+        request.setParameters(parameters);
+        request.executeAsync();
+
+    }
+
+    private void checkLoginStatus(){
+        if(AccessToken.getCurrentAccessToken()!=null){
+            loaduserProfile(AccessToken.getCurrentAccessToken());
+        }
     }
 }
